f4190865db5c6887bc1ad41d498db45ed30176e4
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-18 01:59:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.get("/", (req,res)=>{
 res.send("Your API is working")})
 
 
-cron.schedule('0 6 * * 0', async () => {
-  const response =  await axios.post("https://contest-alert-backened-u4tb-4pw0dm1c3-tusharjain.vercel.app/api/auth/senddata")
+cron.schedule('0 2 * * *', async () => {
+  const response =  await axios.post("https://contest-alert-backened-production.up.railway.app/api/auth/senddata")
 })
 
 cron.schedule('0 14 * * *', async () => {
-  const response =  await axios.post("https://contest-alert-backened-u4tb-4pw0dm1c3-tusharjain.vercel.app/api/auth/")
+  const response =  await axios.post("https://contest-alert-backened-production.up.railway.app/api/auth/reminder")
 })
 
 
