f6ab7ffe2cf29c1ba53625d030cae5f9e7cd7ddb
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-20 18:52:22 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const mongoURI=process.env.REACT_APP_MONGODB_URI
 const connectToMongo = async()=>{
-    await mongoose.connect(mongoURI, ()=>{
+    mongoose.connect(mongoURI, ()=>{
         console.log("Connected to Mongo")
     })
 }
