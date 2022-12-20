2ad37f67e2375087d515a76e71a4a518c1dacb48
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-21 00:39:41 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const mongoURI=process.env.REACT_APP_MONGODB_URI
 const connectToMongo = async()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo")
-    })
+    mongoose.connect(process.env.REACT_APP_MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>
+{console.log("Connected to Mongo")}).catch((err)=>{
+    console.log("Not connected")
+})
 }
 
 module.exports = connectToMongo
