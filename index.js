65c96a4076ef5d4b46fb31eb233add2b0178442a
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-20 17:35:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const app = express()
 // app.use(cors({
 //     origin: "*"
 // }))
-// app.use((req, res, next) => {
-//   res.header({"Access-Control-Allow-Origin": "*"});
-//   next();
-// }) 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
