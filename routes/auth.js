c8f5cb80b51c02884d9bec9517362516aa52a151
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-21 22:48:36 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -139,7 +139,7 @@ router.post("/senddata", async (req, res) => {
           let edt = new Date(element.end_time)
           txt += `<div class="contest_data"> <div class="name"> <span class="h">Contest name </span> <span style="font-family:Verdana, Geneva, Tahoma, sans-serif ; font-size: 17px;font-style: italic;">: ${element.name}</span></div>
             <div class="date"><span class="h">Date : </span> ${(sdt.toDateString())}</div>
-            <div class="time"><span class="h">Time :  </span>${sdt.toLocaleTimeString().slice(0, 5)} to ${edt.toLocaleTimeString().slice(0, 5)} </div>
+            <div class="time"><span class="h">Time :  </span>${sdt.toTimeString().slice(0, 5)} to ${edt.toTimeString().slice(0, 5)} </div>
             <div class="duration"><span class="h">Duration : </span> ${secondsToTime(element.duration)}</div>
             <div class="link"><span class="h">Link : </span><a href="${element.url}">Click Here</a></div>
             <hr></div>`;
@@ -236,7 +236,7 @@ router.post("/reminder", async (req, res) => {
         let edt = new Date(element.end_time)
         txt += `<div class="contest_data"> <div class="name"> <span class="h">Contest name </span> <span style="font-family:Verdana, Geneva, Tahoma, sans-serif ; font-size: 17px;font-style: italic;">: ${element.name}</span></div>
             <div class="date"><span class="h">Date : </span> ${(sdt.toDateString())}</div>
-            <div class="time"><span class="h">Time :  </span>${sdt.toLocaleTimeString().slice(0, 5)} to ${edt.toLocaleTimeString().slice(0, 5)} </div>
+            <div class="time"><span class="h">Time :  </span>${sdt.toTimeString().slice(0, 5)} to ${edt.toTimeString().slice(0, 5)} </div>
             <div class="duration"><span class="h">Duration : </span> ${secondsToTime(element.duration)}</div>
             <div class="link"><span class="h">Link : </span><a href="${element.url}">Click Here</a></div>
             <hr></div>`;
