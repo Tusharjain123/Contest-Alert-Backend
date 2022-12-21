9cef1c86f7c67ffb81e80abfaf8cdbca3d636fb4
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-21 16:05:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,10 @@ app.use((req, res, next) => {
     }
     next();
 });
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json())
+app.use(express.urlencoded())
 app.use("/api/auth", require("./routes/auth.js"))
 
 app.get("/", (req,res)=>{
