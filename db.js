f9d3b9bdff85fbf38eb42434cab6c59c1d957268
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-21 15:28:04 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoURI=process.env.REACT_APP_MONGODB_URI
 const connectToMongo = async()=>{
     mongoose.connect(process.env.REACT_APP_MONGODB_URI, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>
 {console.log("Connected to Mongo")}).catch((err)=>{
-    console.log("Not connected")
+    console.log(err)
 })
 }
 
