9efa72f7dc7eddc8fe121473603275845acd04e6
Tushar <103761473+Tusharjain123@users.noreply.github.com>
2022-12-21 15:58:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const app = express()
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Content-Type','application/json')
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
